a71d73846ce0e92f61d3105ffb799c1444b368ff
Monjep <35704694+Monjep@users.noreply.github.com>
2018-04-03 22:02:06 -0500

    Add project files.

---
+++ Assignment7/Models/Model.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment7.Models
+{
+    public class ModelContext: DbContext
+
+    { 
+        public ModelContext(DbContextOptions<ModelContext> options)
+            : base(options)
+        {}
+        public DbSet<User> Users { get; set; }
+        public DbSet<Notes> notes { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+
+        public class User
+
+        {
+            public int userId { get; set; }
+            public string Email { get; set; }
+            public string Name{ get; set; }
+            public string CreatedOn { get; set; }
+
+        }
+
+
+        public class Notes
+        {
+            public int id { get; set; }
+            public string Title { get; set; }
+            public string note { get; set; }
+            public string CreatedOn { get; set; }
+            public int categoryId { get; set; }
+            public string Isdeleted { get; set; }
+            public int userId { get; set; }
+            
+
+        }
+
+        public class Category
+        {
+
+            public int id { get; set; }
+            public string Name { get; set; }
+
+
+        }
+
+
+
+    }
+        }
+    
+
+  
+
